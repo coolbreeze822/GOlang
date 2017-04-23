59e979e1acf4dc93979d225d6de9e91c1fe77ed6
Luke Zeimet <lzeimet@wisc.edu>
2017-04-23 13:14:15 -0500

    all code from within nested folders

--- 02_numeralSystems/main.go
+++ 02_numeralSystems/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println(42)
+func main() {
+	fmt.Println(42, "\n")
+	fmt.Printf("%d = %b \n", 42, 42)
+	fmt.Printf("%d = %b = %x \n", 163, 163, 163)
+	fmt.Printf("%d = %b = %x \n", 163, 163, 163)
+	fmt.Printf("%d = %b = %#x \n", 163, 163, 163)
+	fmt.Printf("%d = %b = %#X \n", 163, 163, 163)
+	fmt.Printf("%d \t %b \t  %#X \n", 163, 163, 163)
+	for i := 0; i < 100; i++ {
+		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+	}
+	for i := 0; i < 50; i++ {
+		fmt.Printf("%d \t %b \t %x \t %q \n", i, i, i, i)
+	}
+
 }
