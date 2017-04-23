f9d9582fc5ce240f14d40705908754c1e853049f
Luke Zeimet <lzeimet@wisc.edu>
2017-04-23 13:07:46 -0500

    2nd commit, learning some shit

---
+++ 02_numeralSystems/03_hexadecimal/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d = %b = %x \n", 163, 163, 163)
+	fmt.Printf("%d = %b = %x \n", 163, 163, 163)
+	fmt.Printf("%d = %b = %#x \n", 163, 163, 163)
+	fmt.Printf("%d = %b = %#X \n", 163, 163, 163)
+	fmt.Printf("%d \t %b \t  %#X \n", 163, 163, 163)
+}
